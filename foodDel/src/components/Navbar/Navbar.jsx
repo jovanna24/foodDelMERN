4451a3640966d92f289c420a10e1addc683bd9ac
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-07-25 00:19:40 -0700

    added navbar components & style

---
+++ foodDel/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import './Navbar.css'
+import { useState } from 'react'
+import { assets } from '../../assets/images/assets'
+
+const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt='logo' />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")}className={menu==="contact-us"?"active":""}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
