1f25320ae5933935015e66ac5f780a62f982d464
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-07-31 21:00:24 -0700

    working on admin

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://jovannajimenez1124:08241100@cluster0.1ij9ivl.mongodb.net/food-del").then(()=>console.log("DB Connected"));
+    await mongoose.connect("mongodb+srv://jovannajimenez1124:08241100@cluster0.1ij9ivl.mongodb.net/food-del").then(()=>console.log("DB Connected!"));
 }
