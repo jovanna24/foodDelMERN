80cf9a8ef20f1a05ead1a12f27e66162a0f02567
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-02 17:00:52 -0700

    need to make the list data appear on list page

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const List = () => {
+
+  const [list, setList]= useState([]);
+  const url = "http://localhost:4000";
+  const fetchList = async()=>{
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success){
+      setList(response.data.data)
+    } else {
+      toast.error("Error in fetching list")
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
     <div>
     </div>
