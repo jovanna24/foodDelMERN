71b4516a6ef7151fa11ccf4f698dc001809c0e1d
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-03 18:43:08 -0700

    fixing stripe component

--- foodDel/src/context/StoreContext.jsx
+++ foodDel/src/context/StoreContext.jsx
@@ -43,22 +43,58 @@ const StoreContextProvider = (props) => {
     setFoodList(response.data.data)
   };
 
+  /**
+   * This function loads the cart data from the server and updates the cartItems state variable.
+   * It takes a token as a parameter which is used to authenticate the request to the server.
+   * The function makes a POST request to the server at the '/api/cart/get' endpoint with an empty
+   * body and the token as the authorization header. The response from the server is expected to
+   * contain a cartData property which contains the cart items. This function then updates the
+   * cartItems state variable with the cartData from the response.
+   *
+   * @param {string} token - The authentication token for the request.
+   * @return {Promise<void>} A promise that resolves once the cart data has been loaded and the state variable has been updated.
+   */
   const loadCartData = async (token) => {
-    const response = await axios.post(url+"/api/cart/get", {}, {headers: {token}});
-    setCartItems(response.data.cartData);
+    // Make a POST request to the server to get the user's cart data
+    const response = await axios.post(url+"/api/cart/get", {}, {
+      // The headers property is an object that contains the authorization header
+      headers: {
+        // The authorization header is set to the token parameter
+        token
+      }
+    });
+    // The cartData property is extracted from the response and used to update the state variable
+    setCartItems(response.data.cartData); 
   }
 
   useEffect(()=>{
-    
+    /**
+     * This function is responsible for loading the initial data for the app.
+     * It first calls the fetchFoodList function to get the list of available food items
+     * from the server. Then, it checks if there is a token stored in the localStorage.
+     * If there is a token, it sets the token in the state using the setToken function.
+     * After that, it calls the loadCartData function to get the user's cart data from
+     * the server and update the cartItems state variable.
+     *
+     * @return {Promise<void>} A promise that resolves once the initial data has been loaded.
+     */
     async function loadData(){
+      // Fetch the list of available food items from the server
       await fetchFoodList();
+      
+      // Check if there is a token stored in the localStorage
       if (localStorage.getItem("token")){
-      setToken(localStorage.getItem("token"));
-      await loadCartData(localStorage.getItem("token"));
-    }
+        // If there is a token, set it in the state using the setToken function
+        setToken(localStorage.getItem("token"));
+        
+        // Call the loadCartData function to get the user's cart data from the server
+        // and update the cartItems state variable
+        await loadCartData(localStorage.getItem("token"));
+      }
     }
     loadData();
   }, [])
+
   const contextValue = {
     food_list,
     cartItems,
