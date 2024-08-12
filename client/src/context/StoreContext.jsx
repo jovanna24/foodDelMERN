f39a2f84a9b119814a69339f6b59d5094816c0e2
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-12 10:57:33 -0700

    troubleshooting render issues

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -6,7 +6,6 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
