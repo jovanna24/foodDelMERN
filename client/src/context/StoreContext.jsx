1043c4e12baf35a89a288a1e4642cd10da784681
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-12 11:02:29 -0700

    defined url

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -6,6 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "https://fooddelmern.onrender.com/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
